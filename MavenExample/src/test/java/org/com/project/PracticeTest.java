aee05f822ce07a6214a5dc4544095681a7f4a944
Admin <Admin@DESKTOP-SVK5H14>
2022-07-19 11:43:53 +0530

    1st commit

---
+++ MavenExample/src/test/java/org/com/project/PracticeTest.java
@@ -0,0 +1,25 @@
+package org.com.project;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+ @Test
+	 public void demo()
+	 {
+	 String url=System.getProperty("URL");
+	 String browser=System.getProperty("BROWSER");
+	 String username=System.getProperty("USERNAME");
+	 String password=System.getProperty("PASSWORD");
+		System.out.println("Test1-->Test3"); 
+		System.out.println("url====>"+url);
+		System.out.println("browser====>"+browser);
+		System.out.println("username====>"+username);
+		System.out.println("password====>"+password);
+		
+	 }
+
+	@Test		public void demo1() {
+			System.out.println("Test2-->Test3");
+		}
+}
+
---
+++ MavenExample/src/test/java/org/com/project/practice2Test.java
@@ -0,0 +1,18 @@
+package org.com.project;
+
+import org.testng.annotations.Test;
+@Test
+public class practice2Test {
+
+	public class PracticeTest {
+		 @Test
+			 public void demo()
+			 {
+				System.out.println("Test1-->Test3"); 
+			 }
+
+			@Test		public void demo1() {
+					System.out.println("Test2-->Test3");
+				}
+		}
+}
